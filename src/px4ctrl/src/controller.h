6f75e84b8ed34ae248e8d24b381981f09279a3db
edmundwsy <edmundwsy@outlook.com>
2023-07-26 16:47:42 +0200

    rewrite controller

--- src/px4ctrl/src/controller.h
+++ src/px4ctrl/src/controller.h
@@ -9,75 +9,88 @@
 #include <quadrotor_msgs/Px4ctrlDebug.h>
 #include <queue>
 
-#include "input.h"
 #include <Eigen/Dense>
+#include "input.h"
 
-struct Desired_State_t
-{
-	Eigen::Vector3d p;
-	Eigen::Vector3d v;
-	Eigen::Vector3d a;
-	Eigen::Vector3d j;
-	Eigen::Quaterniond q;
-	double yaw;
-	double yaw_rate;
+struct Desired_State_t {
+  Eigen::Vector3d    p;
+  Eigen::Vector3d    v;
+  Eigen::Vector3d    a;
+  Eigen::Vector3d    j;
+  Eigen::Quaterniond q;
+  double             yaw;
+  double             yaw_rate;
 
-	Desired_State_t(){};
+  Desired_State_t(){};
 
-	Desired_State_t(Odom_Data_t &odom)
-		: p(odom.p),
-		  v(Eigen::Vector3d::Zero()),
-		  a(Eigen::Vector3d::Zero()),
-		  j(Eigen::Vector3d::Zero()),
-		  q(odom.q),
-		  yaw(uav_utils::get_yaw_from_quaternion(odom.q)),
-		  yaw_rate(0){};
+  Desired_State_t(Odom_Data_t &odom)
+      : p(odom.p)
+      , v(Eigen::Vector3d::Zero())
+      , a(Eigen::Vector3d::Zero())
+      , j(Eigen::Vector3d::Zero())
+      , q(odom.q)
+      , yaw(uav_utils::get_yaw_from_quaternion(odom.q))
+      , yaw_rate(0){};
 };
 
-struct Controller_Output_t
-{
+struct Controller_Output_t {
+  // Orientation of the body frame with respect to the world frame
+  Eigen::Quaterniond q;
 
-	// Orientation of the body frame with respect to the world frame
-	Eigen::Quaterniond q;
+  // Body rates in body frame
+  Eigen::Vector3d bodyrates;  // [rad/s]
 
-	// Body rates in body frame
-	Eigen::Vector3d bodyrates; // [rad/s]
+  // Collective mass normalized thrust
+  double thrust;
 
-	// Collective mass normalized thrust
-	double thrust;
-
-	//Eigen::Vector3d des_v_real;
+  // Eigen::Vector3d des_v_real;
 };
 
+class ControlBase {
+ public:
+  ControlBase(Parameter_t &param) : param_(param) { resetThrustMapping(); }
+  ~ControlBase(){};
+  virtual quadrotor_msgs::Px4ctrlDebug calculateControl(const Desired_State_t &des,
+                                                        const Odom_Data_t     &odom,
+                                                        const Imu_Data_t      &imu,
+                                                        Controller_Output_t   &u) = 0;
+  virtual bool estimateThrustModel(const Eigen::Vector3d &est_v, const Parameter_t &param);
 
-class LinearControl
-{
-public:
-  LinearControl(Parameter_t &);
-  quadrotor_msgs::Px4ctrlDebug calculateControl(const Desired_State_t &des,
-      const Odom_Data_t &odom,
-      const Imu_Data_t &imu, 
-      Controller_Output_t &u);
-  bool estimateThrustModel(const Eigen::Vector3d &est_v,
-      const Parameter_t &param);
   void resetThrustMapping(void);
 
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
-private:
-  Parameter_t param_;
-  quadrotor_msgs::Px4ctrlDebug debug_msg_;
+ protected:
+  Parameter_t                              param_;
+  quadrotor_msgs::Px4ctrlDebug             debug_msg_;
   std::queue<std::pair<ros::Time, double>> timed_thrust_;
-  static constexpr double kMinNormalizedCollectiveThrust_ = 3.0;
+  static constexpr double                  kMinNormalizedCollectiveThrust_ = 3.0;
 
   // Thrust-accel mapping params
-  const double rho2_ = 0.998; // do not change
-  double thr2acc_;
-  double P_;
+  const double rho2_ = 0.998;  // do not change
+  double       thr2acc_;
+  double       P_;
 
   double computeDesiredCollectiveThrustSignal(const Eigen::Vector3d &des_acc);
   double fromQuaternion2yaw(Eigen::Quaterniond q);
 };
 
+class LinearControl : public ControlBase {
+ public:
+  LinearControl(Parameter_t &param) : ControlBase(param) {}
+  ~LinearControl(){};
+  quadrotor_msgs::Px4ctrlDebug calculateControl(const Desired_State_t &des,
+                                                const Odom_Data_t     &odom,
+                                                const Imu_Data_t      &imu,
+                                                Controller_Output_t   &u) override;
+};
 
 #endif
+
+class GeometricControl : public ControlBase {
+ public:
+  GeometricControl(Parameter_t &param) : ControlBase(param) {}
+  ~GeometricControl(){};
+  quadrotor_msgs::Px4ctrlDebug calculateControl(const Desired_State_t &des,
+                                                const Odom_Data_t     &odom,
+                                                const Imu_Data_t      &imu,
+                                                Controller_Output_t   &u) override;
+};
